d316d212dec6770160582dc6ac69f1c78b1a732c
messyorganised <100069865+messyorganised@users.noreply.github.com>
2023-10-31 23:24:03 +1100

    Add files via upload

---
+++ Microsoft_N_Feature_Updates.ps1
@@ -0,0 +1,74 @@
+﻿# Script to install Windows Update and Feature updates
+
+# Install the Windows Update module if it's not present
+if (-not (Get-InstalledModule -Name "PSWindowsUpdate" -ErrorAction SilentlyContinue)) {
+    Write-Host "
+|========================================|
+
+ Installing the PSWindowsUpdate module...
+
+|========================================|
+    " -ForegroundColor Yellow
+    Install-PackageProvider -Name NuGet -Force
+    Install-Module -Name PSWindowsUpdate -Force
+
+    Write-Host "
+|==============================|
+
+ Module installed Successfully.
+
+|==============================|
+    " -ForegroundColor Green
+
+}
+else {
+    Write-Host "
+|============================================|
+
+ PSWindowsUpdate module is already installed.
+
+|============================================|
+    " -ForegroundColor Green
+}
+
+
+# Check and install Feature updates (Upgrades)
+Write-Host "
+|============================================|
+
+ Checking for Windows and Feature updates...
+
+|============================================|
+" -ForegroundColor Yellow
+
+#Check for any updates via PSWindowsUpdate module
+$allUpdates = Get-WindowsUpdate 
+
+#if Get-WindowsUpdate equals to greater than zero (-gt 0) run the following command. Else will run the command under it.
+if ($allUpdates.Count -gt 0) {
+    Write-Host "
+|=========================================|
+
+ Installing Windows and Feature updates...
+
+|=========================================|
+    " -ForegroundColor Yellow
+    Install-WindowsUpdate -AcceptAll -IgnoreReboot
+}
+else {
+    Write-Host "
+|=============================================|
+
+ No Windows and Feature updates are available.
+
+|=============================================|
+    " -ForegroundColor Green
+}
+
+Write-Host "
+|=======================================================================|
+
+ Windows Update and Feature updates installation completed successfully.
+
+|=======================================================================|
+" -ForegroundColor Green
